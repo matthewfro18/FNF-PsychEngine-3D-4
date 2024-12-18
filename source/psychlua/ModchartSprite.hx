837f1435734b8fef7abf106542949a5d244ffa49
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-12-17 22:10:17 -0500

    Update ModchartSprite.hx

--- source/psychlua/ModchartSprite.hx
+++ source/psychlua/ModchartSprite.hx
@@ -4,7 +4,6 @@ class ModchartSprite extends FlxSprite
 {
 	public var wasAdded:Bool = false;
 	public var animOffsets:Map<String, Array<Float>> = new Map<String, Array<Float>>();
-	public var animOffsets:Map<String, Array<Float>> = new Map<String, Array<Float>>();
 	public function new(?x:Float = 0, ?y:Float = 0)
 	{
 		super(x, y);
