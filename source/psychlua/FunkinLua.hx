2d55279dfaece5c0bb1f6b98b358274af8f1e827
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-12-17 22:16:27 -0500

    Update FunkinLua.hx

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -1858,26 +1858,3 @@ class FunkinLua {
 	}
 }
 #end
-					
-class ModchartView extends Flx3DView
-{
-	public var wasAdded:Bool = false;
-	public function new(?x:Float = 0, ?y:Float = 0, ?w:Int = 0, ?y:Int = 0)
-	{
-		super(x, y, w, y);
-		antialiasing = ClientPrefs.globalAntialiasing;
-	}
-}
-
-class ModchartText extends FlxText
-{
-	public var wasAdded:Bool = false;
-	public function new(x:Float, y:Float, text:String, width:Float)
-	{
-		super(x, y, width, text, 16);
-		setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		cameras = [PlayState.instance.camHUD];
-		scrollFactor.set();
-		borderSize = 2;
-	}
-}
